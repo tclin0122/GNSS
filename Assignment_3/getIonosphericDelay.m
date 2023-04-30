c880e68cc6e3fddbbf079978c779d1986c4018be
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-04-30 16:11:53 +0200

    make it as function form

--- Assignment_3/getIonosphericDelay.m
+++ Assignment_3/getIonosphericDelay.m
@@ -1,6 +1,13 @@
-function [outputArg1,outputArg2] = getIonosphericDelay(inputArg1,inputArg2)
+function [ionosphericDelay] = getIonosphericDelay(TECU,Zenit_angle,Height)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% TECU=5.3;
+%constant parameter
+Re=6371000; %m
+f=1575.42*1e6;
+% start function
+TEC=TECU*10^16;
+zen=90-el;
+OF=(1-((Re*sind(Zenit_angle)/(Re+Height)).^2)).^(-0.5)
+ionosphericDelay=(40.3/(f^2))*TEC*OF
 end
