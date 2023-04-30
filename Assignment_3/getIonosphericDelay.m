7425855a1dfe4b7763f6930170224548be126b84
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-04-30 16:17:43 +0200

    bug fix

--- Assignment_3/getIonosphericDelay.m
+++ Assignment_3/getIonosphericDelay.m
@@ -1,13 +1,16 @@
-function [ionosphericDelay] = getIonosphericDelay(TECU,Zenit_angle,Height)
+function [ionosphericDelay] = getIonosphericDelay(TECU,Zenit_angle)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 % TECU=5.3;
 %constant parameter
 Re=6371000; %m
 f=1575.42*1e6;
+hi=350000; %m
+
 % start function
 TEC=TECU*10^16;
 zen=90-el;
-OF=(1-((Re*sind(Zenit_angle)/(Re+Height)).^2)).^(-0.5)
+OF=(1-((Re*sind(Zenit_angle)/(Re+hi)).^2)).^(-0.5)
 ionosphericDelay=(40.3/(f^2))*TEC*OF
+
 end
