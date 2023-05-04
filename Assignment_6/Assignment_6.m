2bb5153d78817252c9a33e4f0693f5ec02ffc8e8
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-05-04 15:45:09 +0200

    Step 1 to step 9

--- Assignment_6/Assignment_6.m
+++ Assignment_6/Assignment_6.m
@@ -1,6 +1,19 @@
-
+load("data.m")
 %parameters 
-lam %lambda
+lamb=lambda_1 %lambda
+RoDot=sat_data.Ro_dot
+P1a= sat_data.P1_REF
+ph_sa1= sat_data.FI_REF
+P1b= sat_data.P1_ROV
+ph_sb1= sat_data.FI_ROV
+
+da=rec_data.rec_clock_corr(1)
+db=rec_data.rec_clock_corr(2)
+
+roROV=sat_data.Ro_ROV
+roREF=sat_data.Ro_REF
+
+P=[[P_phase;zeros(5)] [zeros(5);P_code]]
 % lambda 1 from table
 
 % step 1 phase correction and code correction
@@ -40,12 +53,45 @@ ay=-(Ys-Yb)/roROV
 az=-(Zs-Zb)/roROV
     %location from satellites and rover
     %roROV [m] from table
-
+ro_ab0=roROV-roREF
 %step 5 double difference 
 ax_d=ax-ax(4)
 ay_d=ay-ay(4)
 az_d=az-az(4)
 
+ax_d(ax_d==0)=[];
+ay_d(ay_d==0)=[];
+az_d(az_d==0)=[];
+
+ro_ab0_d=ro_ab0-ro_ab0(4);
+
+ro_ab0_d(ro_ab0_d==0)=[];
+
+
+% step 6 build A matrix and vector L
+
+
+L=[lam*ph_sab_d-ro_ab0_d ; Psab_d-ro_ab0_d ]
+
+
+amid=[ax ay az]
+A=[[amid;amid] [diag(lam*[1 1 1 1 1]); zeros(5)]]
+X_v=inv(A'*P*A)*A'*P*L
+
+%step 7 Correct the approximate position of the rover
+X=Xb+X_v(1)
+Y=Yb+X_v(2)
+Z=Zb+X_v(3)
+
+%step 8 variance-covariance matrix
+
+Qahat=inv(A'*P*A) %Qx is 8*8 matrix, to get ambiguity fetch 5*5 from the rest part of Qx
+ahat=Qx([4:8],[4:8])
+
+%step 9 use the LAMBDA function
+[afixed,sqnorm,Ps,Qzhat,Z,nfixed,mu]=LAMBDA(ahat,Qahat);
+
+%step 10 
 
 
 
